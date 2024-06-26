b8657d54bd28345d09a34ffac1883b8ac3ab469e
Hamish Croser <h.croser1@gmail.com>
2024-06-26 11:49:04 +1000

    Added spacy conversion and matching

--- atap_corpus_slicer/CorpusSlicer.py
+++ atap_corpus_slicer/CorpusSlicer.py
@@ -1,12 +1,11 @@
 import logging
 import traceback
-from io import BytesIO
 from logging.handlers import RotatingFileHandler
 from os.path import join, dirname, abspath
-from typing import Optional, Callable, Any, Type
+from typing import Optional, Callable, Any, Type, Union
 
-import numpy as np
 import panel as pn
+import spacy
 import param
 from atap_corpus.corpus.base import BaseCorpora
 from atap_corpus.corpus.corpus import DataFrameCorpus
@@ -16,24 +15,31 @@ from panel import Row, Column
 from panel.theme import Fast
 
 from atap_corpus_loader import CorpusLoader, EventType
-from panel.widgets import FileDownload, Tqdm, Button
+from panel.widgets import Tqdm, Button, TextInput
+from spacy import Language
+from spacy.tokens import Doc
+from tqdm import tqdm
 
-from atap_corpus_slicer.Operation import DefaultOperations, DATATYPE_OPERATIONS_MAP, Operations, DataType
+from atap_corpus_slicer.Operation import DefaultOperations, SpacyOperations, DATATYPE_OPERATIONS_MAP, Operations, \
+    DataType, TextOperations
 
 pn.extension(notifications=True, design=Fast)
+tqdm.pandas()
 
 
 class FilterParams(param.Parameterized):
     negation = param.Boolean(label='Negate', default=False, instantiate=True)
     selected_label = param.Selector(label='Data label', instantiate=True)
 
-    def __init__(self, selected_corpus: DataFrameCorpus, **params):
+    def __init__(self, selected_corpus: DataFrameCorpus, nlp: Optional[spacy.Language] = None, **params):
         super().__init__(**params)
         self.selected_operations = DefaultOperations(Series())
+        self.nlp: Optional[spacy.Language] = nlp
+        self.spacy_filter: bool = (self.nlp is not None) and selected_corpus.uses_spacy()
 
         self.remove_filter_button = Button(
             name="Remove",
-            button_type="warning", button_style="outline"
+            button_type="warning", button_style="outline", align="end"
         )
 
         self.panel = Row()
@@ -45,10 +51,11 @@ class FilterParams(param.Parameterized):
         return self.panel
 
     def _update_panel(self):
-        self.panel.objects = [self.param.selected_label,
-                              self.selected_operations,
-                              self.param.negation,
-                              self.remove_filter_button]
+        objects = []
+        if not self.spacy_filter:
+            objects.append(self.param.selected_label)
+        objects.extend([self.selected_operations, self.param.negation, self.remove_filter_button])
+        self.panel.objects = objects
 
     def update_corpus(self, new_corpus: DataFrameCorpus):
         self.selected_corpus = new_corpus
@@ -65,21 +72,27 @@ class FilterParams(param.Parameterized):
 
     @param.depends('selected_label', watch=True)
     def _set_operations(self, *_):
-        logger = logging.getLogger(__name__)
         if self.selected_corpus is None:
             return
         df: DataFrame = self.selected_corpus.to_dataframe()
         selected_data_series: Series = df[self.selected_label]
+        if self.spacy_filter:
+            self.selected_operations = SpacyOperations(self.nlp, selected_data_series)
+            self._update_panel()
+            return
+        elif self.selected_corpus.uses_spacy() and (self.selected_label == self.selected_corpus._COL_DOC):
+            self.selected_operations = TextOperations(selected_data_series)
+            self._update_panel()
+            return
+
         label_datatype_str: str = str(df.dtypes.get(self.selected_label)).lower()
         try:
             label_datatype: DataType = DataType(label_datatype_str)
             operations_type: Type[Operations] = DATATYPE_OPERATIONS_MAP[label_datatype]
+            self.selected_operations = operations_type(selected_data_series)
         except ValueError as e:
-            logger.debug(f"{type(e)}: {str(e)}")
             self.selected_operations = DefaultOperations(selected_data_series)
-            return
 
-        self.selected_operations = operations_type(selected_data_series)
         self._update_panel()
 
     def resolve_filter(self, data_value: Any) -> bool:
@@ -92,8 +105,9 @@ class FilterParams(param.Parameterized):
 class CorpusSlicerParams(param.Parameterized):
     selected_corpus = param.Selector(default=None)
 
-    def __init__(self, **params):
+    def __init__(self, nlp: spacy.Language, **params):
         super().__init__(**params)
+        self.nlp: spacy.Language = nlp
         self.filters: list = []
 
         self.add_filter_button = Button(
@@ -103,8 +117,15 @@ class CorpusSlicerParams(param.Parameterized):
             align='end'
         )
         self.add_filter_button.on_click(self.add_filter)
+        self.add_advanced_filter_button = Button(
+            name="Add advanced filter",
+            button_type="primary", button_style="solid",
+            visible=False,
+            align='end'
+        )
+        self.add_advanced_filter_button.on_click(lambda e: self.add_filter(e, True))
 
-        self.control_row = Row(self.param.selected_corpus, self.add_filter_button)
+        self.control_row = Row(self.param.selected_corpus, self.add_filter_button, self.add_advanced_filter_button)
         self.panel = Column(self.control_row)
 
     def __panel__(self):
@@ -113,15 +134,17 @@ class CorpusSlicerParams(param.Parameterized):
     def reset_filters(self):
         self.on_corpus_update()
 
-    def add_filter(self, *_):
-        new_filter_param = FilterParams(self.selected_corpus)
+    def add_filter(self, event=None, advanced: bool = False):
+        if advanced:
+            new_filter_param = FilterParams(self.selected_corpus, nlp=self.nlp)
+        else:
+            new_filter_param = FilterParams(self.selected_corpus)
         self.filters.append(new_filter_param)
 
         new_filter_param.remove_filter_button.on_click(lambda *_, filter_param=new_filter_param, r=new_filter_param.__panel__(): self.remove_filter_row(filter_param, r))
 
         objects = self.panel.objects
         objects.append(new_filter_param)
-
         self.panel.objects = objects
 
     def remove_filter_row(self, filter_param: FilterParams, filter_row: Row, *_):
@@ -133,19 +156,68 @@ class CorpusSlicerParams(param.Parameterized):
     def on_corpus_update(self):
         if self.selected_corpus is not None:
             self.add_filter_button.visible = True
+            self.add_advanced_filter_button.visible = self.selected_corpus.uses_spacy()
+            advanced: bool = True
+        else:
+            self.add_advanced_filter_button.visible = False
+            advanced: bool = False
         self.filters = []
+        self.panel.objects = []
         self.panel.objects = [self.control_row]
-        self.add_filter()
+        self.add_filter(advanced=advanced)
+
+
+@Language.component("gather_attributes")
+def gather_attributes(doc: Doc):
+    if not len(doc):
+        doc._.attr_vals = {}
+        doc._.custom_attr_vals = {}
+        return doc
+
+    attribute_values: dict[str, set[str]] = {}
+    for attr in dir(doc[0]):
+        is_method = callable(getattr(doc[0], attr))
+        if (not attr.startswith('_')) and (not is_method):
+            attribute_values[attr] = set()
+    custom_attribute_values: dict[str, set[str]] = {}
+    for custom_attr in dir(doc[0]._):
+        is_method = callable(getattr(doc[0]._, custom_attr))
+        if not is_method:
+            custom_attribute_values[custom_attr] = set()
+
+    for tok in doc:
+        for attr in attribute_values.keys():
+            value = getattr(tok, attr)
+            try:
+                attribute_values[attr].add(value)
+            except TypeError:
+                pass
+        for attr in custom_attribute_values.keys():
+            value = getattr(tok._, attr)
+            try:
+                custom_attribute_values[attr].add(value)
+            except TypeError:
+                pass
+
+    doc._.attr_vals = attribute_values
+    doc._.custom_attr_vals = custom_attribute_values
+    return doc
 
 
 class CorpusSlicer(pn.viewable.Viewer):
-    LOGGER: logging.Logger = None
+    LOGGER_NAME: str = "corpus-slicer"
 
     @staticmethod
-    def setup_logger():
-        if CorpusSlicer.LOGGER is not None:
+    def setup_logger(logger_name: str, run_logger: bool):
+        logger = logging.getLogger(logger_name)
+        logger.propagate = False
+        for handler in logger.handlers[:]:
+            logger.removeHandler(handler)
+        if not run_logger:
+            logger.addHandler(logging.NullHandler())
             return
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        formatter = logging.Formatter('%(asctime)s %(levelname)6s - %(name)s:%(lineno)4d %(funcName)20s() - %(message)s')
         log_file_location = abspath(join(dirname(__file__), '..', 'log.txt'))
         # Max size is ~10MB with 1 backup, so a max size of ~20MB for log files
         max_bytes: int = 10000000
@@ -154,38 +226,74 @@ class CorpusSlicer(pn.viewable.Viewer):
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(formatter)
 
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.ERROR)
-        console_handler.setFormatter(formatter)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
 
-        CorpusSlicer.LOGGER = logging.getLogger(__name__)
-        CorpusSlicer.LOGGER.setLevel(logging.DEBUG)
-        CorpusSlicer.LOGGER.addHandler(file_handler)
-        CorpusSlicer.LOGGER.addHandler(console_handler)
+        logger.info('Logger started')
 
-        CorpusSlicer.LOGGER.info('Logger started')
+    @staticmethod
+    def log(msg: str, level: int):
+        logger = logging.getLogger(CorpusSlicer.LOGGER_NAME)
+        logger.log(level, msg)
 
-    def __init__(self, root_directory: str = './', **params):
+    @staticmethod
+    def setup_nlp(nlp: Union[str, spacy.Language]):
+        if isinstance(nlp, str):
+            nlp = spacy.load(nlp)
+        if not isinstance(nlp, spacy.Language):
+            raise TypeError(f"Expected supplied nlp to be Spacy Language, instead got {type(nlp)}")
+
+        if Doc.has_extension("attr_vals"):
+            Doc.remove_extension("attr_vals")
+        if Doc.has_extension("custom_attr_vals"):
+            Doc.remove_extension("custom_attr_vals")
+        if nlp.has_pipe("gather_attributes"):
+            nlp.remove_pipe("gather_attributes")
+        Doc.set_extension("attr_vals", default=None)
+        Doc.set_extension("custom_attr_vals", default=None)
+        nlp.add_pipe("gather_attributes", last=True)
+
+        return nlp
+
+    def __init__(self,
+                 root_directory: str = './',
+                 nlp: Union[str, spacy.Language] = "en_core_web_sm",
+                 run_logger: bool = False,
+                 **params):
         super().__init__(**params)
 
-        CorpusSlicer.setup_logger()
+        CorpusSlicer.setup_logger(CorpusSlicer.LOGGER_NAME, run_logger)
+        self.nlp = self.setup_nlp(nlp)
 
         self.progress_bar = Tqdm(visible=False)
         self.progress_bar.pandas()
-        self.slicer_params = CorpusSlicerParams()
+        self.slicer_params = CorpusSlicerParams(nlp=self.nlp)
+
+        self.run_spacy_button = Button(
+            name="Convert to advanced corpus",
+            button_type="primary", button_style="solid",
+            visible=False,
+            align="start"
+        )
+        self.run_spacy_button.on_click(self.run_spacy)
 
         self.slice_corpus_button = Button(
             name="Slice",
             button_type="success", button_style="solid",
             height=30, width=100,
             visible=False,
-            align="start"
+            align="end"
         )
         self.slice_corpus_button.on_click(self.slice_corpus)
 
+        self.sliced_name_field = TextInput(name='Name', placeholder='Enter a name (leave blank to autogenerate)',
+                                           visible=False)
+
         self.slicer_panel = pn.panel(pn.Column(self.slicer_params,
                                                self.progress_bar,
-                                               self.slice_corpus_button,
+                                               self.run_spacy_button,
+                                               Row(self.slice_corpus_button,
+                                                   self.sliced_name_field),
                                                height=500))
 
         self.corpus_loader: CorpusLoader = CorpusLoader(root_directory)
@@ -200,6 +308,20 @@ class CorpusSlicer(pn.viewable.Viewer):
     def __panel__(self):
         return self.corpus_loader
 
+    def get_corpus_loader(self) -> CorpusLoader:
+        return self.corpus_loader
+
+    def get_mutable_corpora(self) -> BaseCorpora:
+        return self.corpora
+
+    def display_error(self, error_msg: str):
+        self.log(f"Error displayed: {error_msg}", logging.DEBUG)
+        pn.state.notifications.error(error_msg, duration=0)
+
+    def display_success(self, success_msg: str):
+        self.log(f"Success displayed: {success_msg}", logging.DEBUG)
+        pn.state.notifications.success(success_msg, duration=3000)
+
     def set_corpus_selector_value(self, corpus_dict: dict[str, DataFrameCorpus]):
         formatted_dict: dict[str, DataFrameCorpus] = {}
         for name, corpus in corpus_dict.items():
@@ -210,16 +332,53 @@ class CorpusSlicer(pn.viewable.Viewer):
         self.slicer_params.param.selected_corpus.objects = formatted_dict
         if len(corpus_dict):
             self.slicer_params.selected_corpus = list(corpus_dict.values())[-1]
-            self.slice_corpus_button.visible = True
+            corpus_exists = True
+            corpus_uses_spacy = self.slicer_params.selected_corpus.uses_spacy()
+        else:
+            self.slicer_params.selected_corpus = None
+            corpus_exists = False
+            corpus_uses_spacy = False
+
+        self.slice_corpus_button.visible = corpus_exists
+        self.sliced_name_field.visible = corpus_exists
+        self.run_spacy_button.visible = corpus_exists
+        self.run_spacy_button.disabled = corpus_exists and corpus_uses_spacy
+
         self.slicer_params.on_corpus_update()
 
     def on_corpora_update(self, *_):
         if self.corpus_loader is None:
             return
-        corpus_dict: dict[str, DataFrameCorpus] = self.corpus_loader.get_corpora()
+        corpus_dict: dict[str, DataFrameCorpus] = {}
+        corpora_list: list = self.corpora.items()
+        for corpus in corpora_list:
+            corpus_dict[corpus.name] = corpus
+
         self.set_corpus_selector_value(corpus_dict)
 
+    def run_spacy(self, *_):
+        try:
+            self.run_spacy_button.button_style = "outline"
+            if self.slicer_params.selected_corpus is not None:
+                self.slicer_params.selected_corpus.run_spacy(self.nlp)
+        except Exception as e:
+            self.run_spacy_button.button_style = "solid"
+            self.log(traceback.format_exc(), logging.ERROR)
+            self.display_error(f"Error converting corpus: {e}")
+            return
+
+        try:
+            self.slicer_params.on_corpus_update()
+        except Exception as e:
+            self.log(traceback.format_exc(), logging.ERROR)
+            self.display_error(f"Error converting corpus: {e}")
+        self.run_spacy_button.button_style = "solid"
+        self.run_spacy_button.disabled = True
+        self.display_success("Corpus converted to advanced successfully")
+
     def slice_corpus(self, *_):
+        new_name = self.sliced_name_field.value_input
+
         try:
             self.progress_bar.visible = True
             self.slice_corpus_button.button_style = "outline"
@@ -235,9 +394,10 @@ class CorpusSlicer(pn.viewable.Viewer):
                 filter_mask = selected_series.progress_apply(cond_func)
                 filter_bool_mask = filter_mask.astype('bool')
                 mask = filter_bool_mask & mask
-
-            sliced_corpus = corpus.cloned(mask)
-
+            if len(new_name):
+                sliced_corpus = corpus.cloned(mask, new_name)
+            else:
+                sliced_corpus = corpus.cloned(mask)
             self.corpora.add(sliced_corpus)
 
             self.on_corpora_update()
@@ -245,7 +405,10 @@ class CorpusSlicer(pn.viewable.Viewer):
 
             self.slice_corpus_button.button_style = "solid"
             self.progress_bar.visible = False
-        except Exception:
-            self.LOGGER.error(traceback.format_exc())
+            self.sliced_name_field.value = ""
+            self.display_success("Corpus sliced successfully")
+        except Exception as e:
             self.slice_corpus_button.button_style = "solid"
             self.progress_bar.visible = False
+            self.log(traceback.format_exc(), logging.ERROR)
+            self.display_error(f"Error slicing corpus: {e}")
--- atap_corpus_slicer/Operation.py
+++ atap_corpus_slicer/Operation.py
@@ -1,7 +1,9 @@
 import re
 from enum import Enum
-from typing import Callable, Type, Any
+from typing import Callable, Type, Any, Union
 
+import spacy
+from spacy.tokens import Doc
 import pandas as pd
 import param
 import panel as pn
@@ -20,7 +22,7 @@ class Operations(param.Parameterized):
 
 
 class DefaultOperations(Operations):
-    query_value = param.String()
+    query_value = param.String(per_instance=True)
 
     def __panel__(self):
         return pn.Row("is equal to", self.param.query_value)
@@ -71,22 +73,30 @@ class TextOperations(Operations):
                                            "regex search": regex_match}
 
     operation = param.Selector(objects=OPERATIONS_MAP)
-    query_value = param.String()
-    ignore_case = param.Boolean()
+    query_value = param.String(per_instance=True)
+    ignore_case = param.Boolean(per_instance=True)
+
+    def __init__(self, data_series: Series, **params):
+        super().__init__(data_series, **params)
+
 
     def __panel__(self):
         return pn.Row(self.param.operation, self.param.query_value, self.param.ignore_case)
 
+    # @param.depends('operation')
+
+
     @param.depends('query_value', 'ignore_case')
-    def call_operation(self, data_value: str) -> bool:
+    def call_operation(self, data_value: Union[str, Doc]) -> bool:
+        if isinstance(data_value, Doc):
+            data_value = data_value.text_with_ws
         return self.operation(data_value, self.query_value, self.ignore_case)
 
 
 class IntegerOperations(Operations):
-    data_range = pn.widgets.EditableRangeSlider(step=1)
-
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
+        self.data_range = pn.widgets.EditableRangeSlider(step=1)
         self.data_range.start = data_series.min()
         self.data_range.end = data_series.max()
         self.data_range.value = (self.data_range.start, self.data_range.end)
@@ -94,16 +104,14 @@ class IntegerOperations(Operations):
     def __panel__(self):
         return pn.Row("is within the range", self.data_range)
 
-    @param.depends('data_range')
     def call_operation(self, data_value: int) -> bool:
         return self.data_range.value[0] <= data_value <= self.data_range.value[1]
 
 
 class FloatOperations(Operations):
-    data_range = pn.widgets.EditableRangeSlider()
-
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
+        self.data_range = pn.widgets.EditableRangeSlider()
         self.data_range.start = data_series.min()
         self.data_range.end = data_series.max()
         self.data_range.value = (self.data_range.start, self.data_range.end)
@@ -111,13 +119,12 @@ class FloatOperations(Operations):
     def __panel__(self):
         return pn.Row("is within the range", self.data_range)
 
-    @param.depends('data_range')
     def call_operation(self, data_value: float) -> bool:
         return self.data_range.value[0] <= data_value <= self.data_range.value[1]
 
 
 class BooleanOperations(Operations):
-    query_value = param.Selector(objects=[True, False])
+    query_value = param.Selector(per_instance=True, objects=[True, False])
 
     def __panel__(self):
         return pn.Row("is equal to", self.param.query_value)
@@ -128,10 +135,9 @@ class BooleanOperations(Operations):
 
 
 class DateOperations(Operations):
-    date_range = pn.widgets.DatetimeRangePicker()
-
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
+        self.date_range = pn.widgets.DatetimeRangePicker()
         self.date_range.start = data_series.min()
         self.date_range.end = data_series.max()
         self.date_range.value = (self.date_range.start, self.date_range.end)
@@ -139,25 +145,91 @@ class DateOperations(Operations):
     def __panel__(self):
         return pn.Row("is within the range", self.date_range)
 
-    @param.depends('date_range')
     def call_operation(self, data_value: pd.Timestamp) -> bool:
         data_value = data_value.to_pydatetime()
         return self.date_range.value[0] <= data_value <= self.date_range.value[1]
 
 
 class CategoryOperations(Operations):
-    category = pn.widgets.MultiChoice(name='Category')
-
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
+        self.category = pn.widgets.MultiChoice(name='Category')
         self.category.options = list(data_series.unique())
 
     def __panel__(self):
         return pn.Row("is one of", self.category)
 
-    @param.depends('category')
     def call_operation(self, data_value: Any) -> bool:
         return bool(data_value in self.category.value)
+
+
+class SpacyOperations(Operations):
+    attribute = param.Selector(per_instance=True)
+    search = param.String(per_instance=True)
+    ignore_case = param.Boolean(per_instance=True)
+    use_regex = param.Boolean(per_instance=True)
+
+    def __init__(self, nlp: spacy.Language, data_series: Series, **params):
+        super().__init__(data_series, **params)
+        self.nlp: spacy.Language = nlp
+        self.attribute_values = pn.widgets.MultiChoice(align="end")
+        self.param.attribute.objects = self._get_attr_list()
+        self.attribute = self.param.attribute.objects[0]
+
+    def __panel__(self):
+        return pn.Row(self.param.attribute, "is one of", self.attribute_values, "and matches", self.param.search, pn.Column(self.param.ignore_case, self.param.use_regex))
+
+    def _get_attr_list(self) -> list[str]:
+        if not len(self.data_series):
+            return []
+        attr_set = set(self.data_series[0]._.attr_vals.keys())
+        custom_attr_set = set(self.data_series[0]._.custom_attr_vals.keys())
+
+        return sorted(list(attr_set.union(custom_attr_set)))
+
+    def _get_attr_values(self, attribute: str) -> dict[str, Any]:
+        if not len(self.data_series):
+            return {}
+
+        attr_vals: set = set()
+        doc_attr_vals: set[str]
+        for doc in self.data_series:
+            doc_attr_vals = doc._.attr_vals.get(attribute)
+            if doc_attr_vals is None:
+                doc_attr_vals = doc._.custom_attr_vals.get(attribute)
+            attr_vals.update(doc_attr_vals)
+        attr_dict = {str(attr): attr for attr in attr_vals}
+
+        return attr_dict
+
+    @param.depends('attribute', watch=True)
+    def update_tag_values(self):
+        self.attribute_values.options = self._get_attr_values(self.attribute)
+        self.attribute_values.value = []
+
+    @param.depends('attribute', 'search', 'ignore_case', 'use_regex')
+    def call_operation(self, data_value: Doc) -> bool:
+        if len(self.attribute_values.value) == 0:
+            return True
+        regex_flag = 0
+        if self.ignore_case:
+            regex_flag = re.I
+        for token in data_value:
+            if len(self.search):
+                if self.use_regex:
+                    text_match = re.search(self.search, token.text, regex_flag)
+                elif self.ignore_case:
+                    text_match = self.search.lower() in token.text.lower()
+                else:
+                    text_match = self.search in token.text
+            else:
+                text_match = True
+            attribute_match = getattr(token, self.attribute, None) in self.attribute_values.value
+            custom_attribute_match = getattr(token._, self.attribute, None) in self.attribute_values.value
+
+            if text_match and (attribute_match or custom_attribute_match):
+                return True
+        return False
 
 
 class DataType(Enum):
