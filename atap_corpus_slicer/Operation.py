da9ba6f4f8d4eb7ffb80dbe4dbef39d9aab8b34f
Hamish Croser <h.croser1@gmail.com>
2024-06-27 15:49:07 +1000

    Added count input for text contains
    Removed param from Operation

--- atap_corpus_slicer/Operation.py
+++ atap_corpus_slicer/Operation.py
@@ -1,50 +1,57 @@
+import logging
+import operator
 import re
 from enum import Enum
 from typing import Callable, Type, Any, Union
 
 from spacy.tokens import Doc
 import pandas as pd
-import param
 import panel as pn
 from pandas import Series
 
 pn.extension()
 
 
-class Operations(param.Parameterized):
+class Operations:
     def __init__(self, data_series: Series, **params):
         super().__init__(**params)
         self.data_series: Series = data_series
+        self.panel = pn.Row()
+
+    def __panel__(self):
+        return self.panel
 
     def call_operation(self, data_value: Any) -> bool:
         raise NotImplementedError()
 
 
 class DefaultOperations(Operations):
-    query_value = param.String(per_instance=True)
+    def __init__(self, data_series: Series, **params):
+        super().__init__(data_series, **params)
+        self.query_value = pn.widgets.TextInput(name="Search")
+        self.panel.objects = ["is equal to", self.query_value]
 
-    def __panel__(self):
-        return pn.Row("is equal to", self.param.query_value)
-
-    @param.depends('query_value')
     def call_operation(self, data_value: Any) -> bool:
-        return str(data_value) == self.query_value
+        return str(data_value) == self.query_value.value_input
 
 
 class TextOperations(Operations):
+    @staticmethod
+    def text_contains(data_value: str, query_value: str, ignore_case: bool,
+                      count_bound_fn: Callable, count_threshold: int) -> bool:
+        if ignore_case:
+            data_value = data_value.casefold()
+            query_value = query_value.casefold()
+        count = data_value.count(query_value)
+
+        return count_bound_fn(count, count_threshold)
+
     @staticmethod
     def text_equal(data_value: str, query_value: str, ignore_case: bool) -> bool:
         if ignore_case:
             data_value = data_value.casefold()
             query_value = query_value.casefold()
         return data_value == query_value
-
-    @staticmethod
-    def text_contains(data_value: str, query_value: str, ignore_case: bool) -> bool:
-        if ignore_case:
-            data_value = data_value.casefold()
-            query_value = query_value.casefold()
-        return query_value in data_value
 
     @staticmethod
     def starts_with(data_value: str, query_value: str, ignore_case: bool) -> bool:
@@ -67,40 +74,48 @@ class TextOperations(Operations):
             flag = re.I
         return re.search(query_value, data_value, flag) is not None
 
-    OPERATIONS_MAP: dict[str, Callable] = {"equals": text_equal, "contains": text_contains,
-                                           "starts with": starts_with, "ends with": ends_with,
-                                           "regex search": regex_match}
-
-    operation = param.Selector(objects=OPERATIONS_MAP)
-    query_value = param.String(per_instance=True)
-    ignore_case = param.Boolean(per_instance=True)
-
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
 
-    def __panel__(self):
-        return pn.Row(self.param.operation, self.param.query_value, self.param.ignore_case)
+        operations_map: dict[str, Callable] = {"contains": self.text_contains, "equals": self.text_equal,
+                                               "starts with": self.starts_with, "ends with": self.ends_with,
+                                               "regular expression search": self.regex_match}
+        self.operation = pn.widgets.Select(name="Operation", options=operations_map)
+        self.query_value = pn.widgets.TextInput(name="Search")
+        self.count_bound_fn = pn.widgets.Select(options={"at least": operator.ge, "at most": operator.le},
+                                                align="end", visible=False, width=100)
+        self.count_threshold = pn.widgets.IntInput(name="Occurrences", start=1, value=1,
+                                                   align="end", visible=False, width=100)
+        self.ignore_case = pn.widgets.Checkbox(name="Ignore case")
 
-    # @param.depends('operation')
+        self.panel.objects = [self.operation, self.query_value, self.count_bound_fn, self.count_threshold, self.ignore_case,
+                              pn.bind(self.toggle_count_inputs, self.operation)]
 
+    def toggle_count_inputs(self, *_):
+        logger = logging.getLogger("corpus-slicer")
+        logger.info(f"Operation: {self.operation.value}, {id(self.operation.value)}")
+        logger.info(f"Contains operation: {self.text_contains}, {id(self.text_contains)}")
+        count_op = self.operation.value == self.text_contains
+        self.count_bound_fn.visible = count_op
+        self.count_threshold.visible = count_op
 
-    @param.depends('query_value', 'ignore_case')
     def call_operation(self, data_value: Union[str, Doc]) -> bool:
         if isinstance(data_value, Doc):
             data_value = data_value.text_with_ws
-        return self.operation(data_value, self.query_value, self.ignore_case)
+        if self.operation.value == self.text_contains:
+            return self.operation.value(data_value, self.query_value.value_input, self.ignore_case.value, self.count_bound_fn.value, self.count_threshold.value)
+        return self.operation.value(data_value, self.query_value.value_input, self.ignore_case.value)
 
 
 class IntegerOperations(Operations):
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
-        self.data_range = pn.widgets.EditableRangeSlider(step=1)
+        self.data_range = pn.widgets.EditableRangeSlider(name="is within the range", step=1)
         self.data_range.start = data_series.min()
         self.data_range.end = data_series.max()
         self.data_range.value = (self.data_range.start, self.data_range.end)
 
-    def __panel__(self):
-        return pn.Row("is within the range", self.data_range)
+        self.panel.objects = [self.data_range]
 
     def call_operation(self, data_value: int) -> bool:
         return self.data_range.value[0] <= data_value <= self.data_range.value[1]
@@ -109,39 +124,37 @@ class IntegerOperations(Operations):
 class FloatOperations(Operations):
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
-        self.data_range = pn.widgets.EditableRangeSlider()
+        self.data_range = pn.widgets.EditableRangeSlider(name="is within the range")
         self.data_range.start = data_series.min()
         self.data_range.end = data_series.max()
         self.data_range.value = (self.data_range.start, self.data_range.end)
 
-    def __panel__(self):
-        return pn.Row("is within the range", self.data_range)
+        self.panel.objects = [self.data_range]
 
     def call_operation(self, data_value: float) -> bool:
         return self.data_range.value[0] <= data_value <= self.data_range.value[1]
 
 
 class BooleanOperations(Operations):
-    query_value = param.Selector(per_instance=True, objects=[True, False])
+    def __init__(self, data_series: Series, **params):
+        super().__init__(data_series, **params)
+        self.query_value = pn.widgets.Select(name="is equal to", objects=[True, False])
 
-    def __panel__(self):
-        return pn.Row("is equal to", self.param.query_value)
+        self.panel.objects = [self.query_value]
 
-    @param.depends('query_value')
     def call_operation(self, data_value: bool) -> bool:
-        return data_value == self.query_value
+        return data_value == self.query_value.value
 
 
 class DateOperations(Operations):
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
-        self.date_range = pn.widgets.DatetimeRangePicker()
+        self.date_range = pn.widgets.DatetimeRangePicker(name="is within the range")
         self.date_range.start = data_series.min()
         self.date_range.end = data_series.max()
         self.date_range.value = (self.date_range.start, self.date_range.end)
 
-    def __panel__(self):
-        return pn.Row("is within the range", self.date_range)
+        self.panel.objects = [self.date_range]
 
     def call_operation(self, data_value: pd.Timestamp) -> bool:
         data_value = data_value.to_pydatetime()
@@ -151,22 +164,15 @@ class DateOperations(Operations):
 class CategoryOperations(Operations):
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
-        self.category = pn.widgets.MultiChoice(name='Category')
-        self.category.options = list(data_series.unique())
+        self.category = pn.widgets.MultiChoice(name="is one of", options=list(data_series.unique()))
 
-    def __panel__(self):
-        return pn.Row("is one of", self.category)
+        self.panel.objects = [self.category]
 
     def call_operation(self, data_value: Any) -> bool:
         return bool(data_value in self.category.value)
 
 
 class SpacyOperations(Operations):
-    attribute = param.Selector(per_instance=True)
-    search = param.String(per_instance=True)
-    ignore_case = param.Boolean(per_instance=True)
-    use_regex = param.Boolean(per_instance=True)
-
     attr_no_shows: set[str] = {"ancestors", "children", "cluster", "conjuncts", "dep", "doc", "ent_id",
                                "ent_id_", "ent_iob", "ent_iob_", "ent_kb_id", "ent_kb_id_", "ent_type",
                                "has_vector", "head", "i", "idx", "lang", "left_edge", "lefts", "lemma",
@@ -181,12 +187,15 @@ class SpacyOperations(Operations):
 
     def __init__(self, data_series: Series, **params):
         super().__init__(data_series, **params)
-        self.attribute_values = pn.widgets.MultiChoice(align="end")
-        self.param.attribute.objects = self._get_attr_list()
-        self.attribute = list(self.param.attribute.objects.values())[0]
+        self.attribute = pn.widgets.Select(name="Attribute", options=self._get_attr_list())
+        self.attribute_values = pn.widgets.MultiChoice(name="is one of", align="end")
+        self.search = pn.widgets.TextInput(name="matches")
+        self.ignore_case = pn.widgets.Checkbox(name="Ignore case")
+        self.use_regex = pn.widgets.Checkbox(name="Regular expression")
 
-    def __panel__(self):
-        return pn.Row(self.param.attribute, "is one of", self.attribute_values, "and matches", self.param.search, pn.Column(self.param.ignore_case, self.param.use_regex))
+        self.panel.objects = [self.attribute, self.attribute_values, self.search,
+                              pn.Column(self.ignore_case, self.use_regex),
+                              pn.bind(self.update_tag_values, self.attribute)]
 
     def _get_attr_list(self) -> dict[str, str]:
         if not len(self.data_series):
@@ -208,7 +217,7 @@ class SpacyOperations(Operations):
         return attr_dict
 
     def _get_attr_values(self, attribute: str) -> dict[str, Any]:
-        if not len(self.data_series):
+        if (not len(self.data_series)) or (not Doc.has_extension("attr_vals")) or (not Doc.has_extension("custom_attr_vals")):
             return {}
 
         attr_vals: set = set()
@@ -217,35 +226,36 @@ class SpacyOperations(Operations):
             doc_attr_vals = doc._.attr_vals.get(attribute)
             if doc_attr_vals is None:
                 doc_attr_vals = doc._.custom_attr_vals.get(attribute)
+            if doc_attr_vals is None:
+                continue
             attr_vals.update(doc_attr_vals)
         attr_dict = {str(attr): attr for attr in attr_vals}
 
         return attr_dict
 
-    @param.depends('attribute', watch=True)
-    def update_tag_values(self):
-        self.attribute_values.options = self._get_attr_values(self.attribute)
+    def update_tag_values(self, *_):
+        self.attribute_values.options = self._get_attr_values(self.attribute.value)
         self.attribute_values.value = []
 
-    @param.depends('attribute', 'search', 'ignore_case', 'use_regex')
     def call_operation(self, data_value: Doc) -> bool:
         if len(self.attribute_values.value) == 0:
             return True
         regex_flag = 0
         if self.ignore_case:
             regex_flag = re.I
+        search_val = self.search.value_input
         for token in data_value:
-            if len(self.search):
+            if len(search_val):
                 if self.use_regex:
-                    text_match = re.search(self.search, token.text, regex_flag)
+                    text_match = re.search(search_val, token.text, regex_flag)
                 elif self.ignore_case:
-                    text_match = self.search.lower() in token.text.lower()
+                    text_match = search_val.casefold() in token.text.casefold()
                 else:
-                    text_match = self.search in token.text
+                    text_match = search_val in token.text
             else:
                 text_match = True
-            attribute_match = getattr(token, self.attribute, None) in self.attribute_values.value
-            custom_attribute_match = getattr(token._, self.attribute, None) in self.attribute_values.value
+            attribute_match = getattr(token, self.attribute.value, None) in self.attribute_values.value
+            custom_attribute_match = getattr(token._, self.attribute.value, None) in self.attribute_values.value
 
             if text_match and (attribute_match or custom_attribute_match):
                 return True
